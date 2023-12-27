79dea2c641cad37e0e51b3af798f7911475a5b91
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2023-12-28 01:02:14 +0200

    XCode basic project

---
+++ Alpine F1/Alpine_F1App.swift
@@ -0,0 +1,17 @@
+//
+//  Alpine_F1App.swift
+//  Alpine F1
+//
+//  Created by Florian DAVID on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Alpine_F1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
